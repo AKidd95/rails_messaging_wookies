0e69696d655491cef7aad91fa283c7b20fce8865
Aiden_Jubelin <aiden.jubelin@gmail.com>
2018-02-20 17:45:45 +0100

    edited feature file added step_definitions

---
+++ features/step_definitions/user_login_steps.rb
@@ -0,0 +1,19 @@
+Given("user has a mailbox") do
+  visit mailbox
+end
+
+When("the user visits the {string}") do |string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("he should see {string}") do |string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("he clicks {string}") do |string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("he is redirected to {string}") do |string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
---
+++ features/user_login.feature
@@ -0,0 +1,13 @@
+Feature: User can login
+  As a user
+  So that I can login to my account
+  There should be a login-form on the site
+
+  Background:
+    Given user has a mailbox
+
+  Scenario: User visits page
+    When the user visits the 'landing page'
+    Then he should see 'Login button'
+    Then he clicks 'Login button'
+    And he is redirected to 'login page'
